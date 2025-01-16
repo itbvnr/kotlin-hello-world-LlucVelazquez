660d318e2841887d8bf6afd21281577e015fc408
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2025-01-16 10:42:28 +0100

    MessagesListApp Doned

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/MessagesListApp.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/MessagesListApp.kt
@@ -1,10 +1,19 @@
 package cat.itb.m78.exercices
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.draw.clip
 
 data class Message(val author: String, val body: String)
 
@@ -40,10 +49,32 @@ val messages = List(100){
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MessagesListApp() {
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(messages) { index -> Text(index.author)
-        Text(index.body) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("App Bar Title", fontSize = 20.sp) },
+                navigationIcon = { Icon(Icons.AutoMirrored.Filled.ArrowBack, "back") })
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(Icons.Default.Add, "Add")
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier.padding(paddingValues).padding(10.dp)
+        ) {
+            items(messages) { index ->
+                Column(modifier = Modifier.clip(RoundedCornerShape(10.dp)).background(Color.LightGray).padding(25.dp))
+                {
+                    Text(index.author)
+                    Text(index.body)
+                }
+                Spacer(Modifier.height(8.dp))
+            }
+        }
     }
 }
